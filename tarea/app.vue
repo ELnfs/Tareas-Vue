2443be541e5971f8cc38082469dc1c88fe12c959
Emilio Lezama <45559363+ELnfs@users.noreply.github.com>
2019-09-26 14:34:06 +0200

    Add files via upload

---
+++ tarea/app.vue
@@ -0,0 +1,27 @@
+const App = new Vue ({
+    el: "#contenedor",
+    data: {
+        titulo: "Tareas con Vue :)",
+        imagen: "",
+        tareas: [
+            { nombre: "Comprar", estado: true, },
+            { nombre: "Estudiar",  estado: false, },
+            { nombre: "Comer", estado: false, },
+            { nombre: "Pintar", estado: true, },
+        ],
+    },
+    methods: {
+        quitarTarea: function(tarea) {
+            this.tareas.splice(this.tareas.indexOf(tarea),1);
+        },
+        addTarea: function(e) {
+            event.preventDefault();
+            console.log("hola lokaso");
+            this.tareas.push({
+                nombre: this.tareas.nombre,
+                estado: false,
+            });
+            this.tareas.nombre = '';
+        }
+    }
+})
